f96e766fcbea901b365fbee026c7d3604c46ddd4
Brian Underwood <brian@brian-underwood.codes>
2021-04-23 09:30:32 +0200

    Use production config from Heroku ENV variables and decrease socket timeout

--- lib/dice_viz_web/endpoint.ex
+++ lib/dice_viz_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule DiceVizWeb.Endpoint do
   ]
 
   socket "/socket", DiceVizWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
